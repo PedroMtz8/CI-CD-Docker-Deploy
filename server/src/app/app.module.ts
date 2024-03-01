bfe2ff1c27ccfd3051ee68c4a5fe2f95d95b1666
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-01 02:44:23 -0600

    config db connection and env variables

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from '@/config/config.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
