eed80e03b4cd40c2cbe0c41ab87924727f5bfb04
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-01 23:25:55 -0600

    add blogs and blog-card components, get api blogs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Blogs from './components/blogs';
 import Navbar from './components/navbar';
 import GlobalModals from './providers/ModalsProvider';
 
@@ -7,7 +8,7 @@ export default function App() {
     <>
       <GlobalModals />
       <Navbar />
-      <h1 className='text-blue-500' >Hello world</h1>
+      <Blogs />
     </>
   )
 }
