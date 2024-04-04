8136904a443a0beab4adff7c15739fe01e6ee087
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-03 22:50:50 -0600

    logger using fastify adapter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { loggerFactory } from "./config/logger.factory";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -11,12 +12,7 @@ const port = PORT
 
 async function bootstrap() {
   const isProd = true;
-  // const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
-  // const app = await NestFactory.create(AppModule, {
-  //   bufferLogs: true,
-  // })
-
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     logger: WinstonModule.createLogger({
       transports: [
         // let's log errors into its own file
@@ -43,8 +39,41 @@ async function bootstrap() {
           ),
         }),
       ],
-    }),
-  });
+    })
+  })
+  // const app = await NestFactory.create(AppModule, {
+  //   bufferLogs: true,
+  // })
+
+  // const app = await NestFactory.create(AppModule, {
+  //   logger: WinstonModule.createLogger({
+  //     transports: [
+  //       // let's log errors into its own file
+  //       new transports.File({
+  //         filename: `logs/error.log`,
+  //         level: 'error',
+  //         format: format.combine(format.timestamp(), format.json()),
+  //       }),
+  //       // logging all level
+  //       new transports.File({
+  //         filename: `logs/info.log`,
+  //         level: 'info',
+  //         format: format.combine(format.timestamp(), format.json()),
+  //       }),
+  //       // we also want to see logs in our console
+  //       new transports.Console({
+  //         format: format.combine(
+  //           format.cli(),
+  //           format.splat(),
+  //           format.timestamp(),
+  //           format.printf((info) => {
+  //             return `${info.timestamp} ${info.level}: ${info.message}`;
+  //           }),
+  //         ),
+  //       }),
+  //     ],
+  //   }),
+  // });
 
 
   app.useGlobalPipes(new ValidationPipe());
