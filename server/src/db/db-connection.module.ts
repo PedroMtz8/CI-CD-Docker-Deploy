1b5d0b39fbe1b69c87009f9a3c53eef9371ffdf4
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-09 21:11:45 -0600

    change db connection, move to external module

---
+++ server/src/db/db-connection.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
+  ],
+})
+export class TypeOrmConfigModule { }
