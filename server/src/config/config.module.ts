821229383e9c91dca32f4143b70f9a2f587651e8
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-11 19:53:34 -0600

    add env config loader

--- server/src/config/config.module.ts
+++ server/src/config/config.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { configLoader } from './env-config.loader';
 
 @Module({
   imports: [ConfigModule.forRoot({
+    load: [configLoader],
     envFilePath: '.env',
     isGlobal: true
   })],
---
+++ server/src/config/env-config.loader.ts
@@ -0,0 +1,13 @@
+
+export const configLoader = () => {
+  return {
+    port: parseInt(process.env.PORT, 10) || 4000,
+    database: {
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      name: process.env.DATABASE_NAME,
+    },
+  };
+}
