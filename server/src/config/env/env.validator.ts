3a872f53454a164d0ec9a426bd31796f1720d42b
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-04-27 21:04:26 -0600

    add volume for hotreload on develop changes

--- server/src/config/env/env.validator.ts
+++ server/src/config/env/env.validator.ts
@@ -3,8 +3,8 @@ import * as Joi from 'joi';
 
 export const envSchemaValidator = Joi.object({
   PORT: Joi.number().default(4000),
-  DATABASE_HOST: Joi.string().required(),
-  DATABASE_PORT: Joi.number().default(5432),
+  DATABASE_HOST: Joi.string().default('localhost'),
+  DATABASE_PORT: Joi.number().default(3306),
   DATABASE_USER: Joi.string().required(),
   DATABASE_PASSWORD: Joi.string().required(),
   DATABASE_NAME: Joi.string().required(),
